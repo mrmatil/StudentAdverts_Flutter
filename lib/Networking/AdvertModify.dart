7fdb093ef7938a906e1a3cc22ac5ffb6bab5bef3
Mateusz Łukasiński <mat.lukasinski@gmail.com>
2020-05-31 04:58:52 +0200

    Fixed Adding issue

--- lib/Networking/AdvertModify.dart
+++ lib/Networking/AdvertModify.dart
@@ -15,7 +15,6 @@ class AdvertModify{
 
   AdvertModify(this.typeOfModification, this.advertModel, this.callback, this.id);
   void setNewAdvert(){
-    print(id);
     switch(typeOfModification){
       case TypeOfModification.modify:{
         _modifyModel();
@@ -38,14 +37,13 @@ class AdvertModify{
     try{
       var dio = new Dio();
       Response response = await dio.put(baseUrl+_modifyUrl,data: {
-        "id": advertModel.id,
         "title": advertModel.title,
         "author": advertModel.author,
         "email": advertModel.email,
         "phone": advertModel.phone,
-        "dateAndTime": "2020-05-31T00:29:15.3936609+00:00",
+        "dateAndTime": advertModel.dateTime,
         "description": advertModel.description,
-        "price": advertModel.description,
+        "price": advertModel.price,
         "picture": "QEA="
       }, options: Options(
           headers: {
@@ -70,7 +68,7 @@ class AdvertModify{
         "phone": advertModel.phone,
         "dateAndTime": "2020-05-31T00:29:15.3936609+00:00",
         "description": advertModel.description,
-        "price": advertModel.description,
+        "price": advertModel.price,
         "picture": "QEA="
       }, options: Options(
           headers: {
